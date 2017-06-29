99b7aacf30c30d2dd21f3763eaef04d0aad66d17
Phill Conrad <pconrad@cs.ucsb.edu>
2017-06-28 17:51:36 -0700

    try additional logging code

--- webapp.py
+++ webapp.py
@@ -46,6 +46,13 @@ print("About to execute app = Flask(__name__)")
 app = Flask(__name__)
 
 
+if os.environ.get('HEROKU') is not None:
+    import logging
+    stream_handler = logging.StreamHandler()
+    app.logger.addHandler(stream_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('microblog startup')
+
 app.secret_key = os.environ['APP_SECRET_KEY']
 
 oauth = OAuth(app)
